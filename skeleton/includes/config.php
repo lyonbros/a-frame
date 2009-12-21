ac4114a0a3b5140f2a03efcffb459f105ac84d61
Andrew Lyon <orthecreedence@gmail.com>
2009-12-20 16:00:33 -0800

    modified custom routing system to use a library for custom routes instead of a controller. this makes a lot more sense architecture-wise. also added example REST routes library. is more or less the same code from the beeets api, so should work fine.

--- skeleton/includes/config.php
+++ skeleton/includes/config.php
@@ -27,5 +27,5 @@
 	
 	define('CRON_CONTROLLER', 'cron');				// default name for the built-in cron-job controller
 	
-	define('ROUTE_CONTROLLER', false);				// set to a controller in the controllers/ directory to use custom routing
+	define('ROUTE_LIBRARY', false);					// set to a controller in the controllers/ directory to use custom routing
 ?>
