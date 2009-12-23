5de627347d4cec47acd5befd3099f12a42451370
U-LBANDREW\andrew <andrew@LBANDREW.(none)>
2009-12-22 18:16:36 -0800

    added better cron script (includes app index.php instead of including a-frame index directly.)

--- skeleton/scripts/cron.php
+++ skeleton/scripts/cron.php
@@ -1,6 +1,4 @@
 <?
 	define('CRON_JOB', true);
-	$app_base	=	dirname(__FILE__) . '/..';
-	$core_base	=	$app_base . '/../A-Frame';
-	include $core_base . '/index.php';
+	include dirname(__FILE__) . '/../index.php';
 ?>
